cde489959a30d4b85c308da455c7ecd9ddba0923
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-02-16 18:52:47 +0100

    .

---
+++ script.js
@@ -0,0 +1,28 @@
+var url = "https://rickandmortyapi.com/api/character";
+const output = document.querySelector(".container");
+
+const lastPage = document.querySelector("#lastPage");
+const nextPage = document.querySelector("#nextPage");
+
+const listData = (list) => {
+
+    console.log(list.length);
+
+    output.innerHTML = "";
+    
+    for (let item of list) {
+        let newDiv = `
+            <a href="#" id="info">
+                <img src="${item.image}">
+            </a>`
+        output.innerHTML += newDiv;  
+    }
+}
+
+fetch(url)
+.then(response => response.json())
+.then(list => listData(list.results))
+.catch(error => {
+    console.error(error);
+    output.innerHTML = "Something went wrong..."
+});
